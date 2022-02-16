62ddec5822586d515aa156eeb8cf174806887953
Muhammad Osama <94981090+0sama-rao@users.noreply.github.com>
2022-02-16 17:44:41 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var  db = require('./controllers/userController');
-const { pdfController } = require('./controllers/pdfController');
-var  login = require('./login');
+var pdf = require('./controllers/pdfController');
 var  express = require('express');
 var  bodyParser = require('body-parser');
 var  cors = require('cors');
+const HttpError = require('./models/http-error');
 var  app = express();
 var  router = express.Router();
 
@@ -13,7 +13,7 @@ app.use(cors());
 app.use('/api', router);
 
 router.use((request, response, next) => {
-  console.log('middleware');
+  console.log('Method Executed !');
   next();
 });
 
@@ -28,6 +28,9 @@ router.route('/users').get((request, response) => {
 //Get UserInfo by RegNo
 router.route('/user/info/:id').get((request, response) => {
   db.getUserByRegNo(request.params.id).then((data) => {
+    if(data == null){
+      response.json("Data not found",404)
+    }
     response.json(data);
   })
 })
@@ -46,24 +49,50 @@ router.route('/user/BalanceInquiry/:mobile').get((request, response) => {
   })
 })
 
-
-//to list all the users/investors
-router.route('/user/create-pdf').get((request, response) => {
-  pdfController.createInvoice().then((data) => {
-    response.json("Pdf Created", path);
+router.route('/user/FundPrices/:id').get((request, response) => {
+  db.getFundPrices(request.params.id).then((data) => {
+    response.json(data);
   })
 })
-// //list the user with NIC
-// router.route('/user/:id').get((request, response) => {
-//   db.getUserByNIC(request.params.id).then((data) => {
+
+
+// const getFundPrices = (req, res, next) => {
+//   const placeId = req.params.id; //{pid: 'p1'}
+//   const place = accStat.find(p=>{
+//     return p.RegNo === id;
+//   });
+//
+//   if( !place ){
+//     throw new HttpError ('Could not find the place for the provide ID',404);
+//   }
+//   res.json({place}); // => {place } => {place: place}
+// };
+//get Balance Inquirey by MobileNumber
+// router.route('/user/AccountStatement/:mobile').get((request, response) => {
+//   pdfController.createInvoice(request.params.mobile).then(() => {
+//   response.return(path);
+//
+
+
+
+router.route('/user/AccountStatement/:mobile').get((request, response) => {
+  pdf.createPdf(request.params.mobile).then((data)=>{
+    response.json(data);
+  });
+
+  response.json("Pdf Created! ");
+});
+
+// router.route('/user/AccountStatement/:mobile').get((request, response) => {
+//   const pMob = request.params.id;
+//   const pData = createPdf.find(data=>{
 //     response.json(data);
 //   })
-// })
-
-
-
-
-
+//   if (!pData){
+//     throw new Httperror('Not Regiestered! ')
+//   }
+//   response.json(data);
+// });
 
 
 router.route('users/login').post((request, response) => {
